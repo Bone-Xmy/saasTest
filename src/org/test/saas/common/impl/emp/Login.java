84e7f3093529f2d995936f0fda4c3af159098743
xiaomingye <xiaomingye@bone.com>
2018-09-11 00:18:51 +0800

    some optmize

--- src/org/test/saas/common/impl/emp/Login.java
+++ src/org/test/saas/common/impl/emp/Login.java
@@ -5,6 +5,7 @@ import org.test.saas.common.*;
 public class Login implements BaseInterface{
 	private String userName;
 	private String password;
+	private String host;
 
 	public void setUserName(String userName){
 		this.userName = userName;
@@ -18,8 +19,16 @@ public class Login implements BaseInterface{
 	public String getPassword(){
 		return this.password;
 	}
+	public void setHost(String name){
+		this.host = host;
+	}
+	public String getHost(){
+		return this.host;
+	}
 
 	public String[] doing(){
+		String url = getHost() + "/saas/emp/Login.ajax";
+		
 		
 	}
 }
